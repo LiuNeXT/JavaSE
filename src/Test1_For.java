c861c3eb1378694dec58d33c279658790f767135
Nexoliu <34900684+Nexoliu@users.noreply.github.com>
2018-08-23 22:15:41 +0800

    update

---
+++ src/Test1_For.java
@@ -0,0 +1,11 @@
+public class Test1_For {
+    public static void main(String[] args) {
+        int he = 0;
+        for (int i = 1;i <= 10; i++){
+            if ( i % 2 == 0){
+                he = he + i;
+            }
+        }
+        System.out.println(he);
+    }
+}
