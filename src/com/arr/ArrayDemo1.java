83e20219bf0d0ac54ee11eda689ebb0c3ce6b7fe
Nexoliu <34900684+Nexoliu@users.noreply.github.com>
2018-08-25 11:29:39 +0800

    new

---
+++ src/com/arr/ArrayDemo1.java
@@ -0,0 +1,10 @@
+package com.arr;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,55,17,22,28};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+            }
+        }
+    }
---
+++ src/com/base/Test3_OverLoad.java
@@ -0,0 +1,27 @@
+package com.base;
+
+/*
+ * A:案例演示
+ * 需求：比较两个数据是否相等。
+ * 参数类型分别为两个int类型，两个double类型，并在main方法中进行测试
+
+ */
+public class Test3_OverLoad {
+
+    public static void main(String[] args) {
+
+        boolean b1 = isEquals(10,10);
+        System.out.println(b1);
+
+        boolean b2 = isEquals(10.5,10.5);
+        System.out.println(b2);
+    }
+
+    public static boolean isEquals(int a,int b){
+        return a ==b ;
+    }
+
+    public static boolean isEquals(double a,double b){
+        return a == b;
+    }
+}
