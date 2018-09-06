1a392f0613cf5c12b9e362c7eb4a98c2b720615a
Nexoliu <34900684+Nexoliu@users.noreply.github.com>
2018-09-06 16:24:10 +0800

    Array

---
+++ src/com/heima/Base/Day06/Demo2_InnerClass.java
@@ -0,0 +1,25 @@
+package com.heima.Base.Day06;
+
+public class Demo2_InnerClass {
+    public static void main(String[] args) {
+        //Outer.Inner oi = new Outer().new Inner();
+        //oi.method();
+
+        Outer1 o = new Outer1();
+        o.print();
+    }
+}
+
+    class Outer1 {
+    static  int num = 10;
+    static class Inner {
+        public void method() {
+            System.out.println(num);
+        }
+    }
+
+    public void print() {
+        Inner i = new Inner();
+        i.method();
+    }
+}
--- src/com/heima/Base/Day06/Person.java
+++ src/com/heima/Base/Day06/Person.java
@@ -1,33 +1,39 @@
 package com.heima.Base.Day06;
 
 public class Person {
+    //私有属性
     private String name;
     private int age;
 
+    //无参构造
     public Person(){}
-
+    //有参构造
     public  Person(String name,int age){
         this.name = name;
         this.age = age;
     }
-
+    //Get方法
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public int getAge() {
+
         return age;
     }
 
     public void setAge(int age) {
+
         this.age = age;
     }
 
     protected void print(){
+
         System.out.println("Print");
     }
 }
