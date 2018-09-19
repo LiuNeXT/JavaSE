c4b5af932c89f50442189949983267caa664f052
nexoliu <i@nexoit.cn>
2018-09-19 15:34:45 +0800

    commit

--- src/com/heima/Base/Day07/Demo1_Constructor.java
+++ src/com/heima/Base/Day07/Demo1_Constructor.java
@@ -1,7 +1,9 @@
 package com.heima.Base.Day07;
 
 public class Demo1_Constructor {
+
     public static void main(String[] args) {
+
 
     }
 }
---
+++ src/com/heima/Base/Day09/Demo2_InterFace.java
@@ -0,0 +1,18 @@
+package com.heima.Base.Day09;
+
+public class Demo2_InterFace {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Inter2{
+    //public static final int num=10;
+    int number=10;
+
+
+    //public abstract void print2();
+    void print2();
+
+}
+
