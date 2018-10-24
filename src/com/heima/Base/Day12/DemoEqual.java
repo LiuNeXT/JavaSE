f46b2751724c43ded0b5337b9230cb771c330219
nexoliu <i@nexoit.cn>
2018-10-24 16:57:50 +0800

    Day19

--- src/com/heima/Base/Day12/DemoEqual.java
+++ src/com/heima/Base/Day12/DemoEqual.java
@@ -10,7 +10,7 @@ public class DemoEqual {
 
         String s1 = "abc";
 
-        String s2 = "abc";
+            String s2 = "abc";
 
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
